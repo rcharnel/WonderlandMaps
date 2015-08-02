486b6bc2ef208a811276d4c1dca97eee488b3edc
Rcharnel <lilr15@live.com>
2015-08-02 18:58:46 -0400

    Initial Commit

--- 11016/Class1.cs
+++ 11016/Class1.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Game;
+using Plugin;
 
 namespace _11016
 {
-    public class Class1
+    public class map11016:GameMap
     {
+        
+
+        public map11016(object host, object src)
+            : base((PluginHost)host, (System.IO.FileInfo)src)
+        {
+
+        }
+        
+        public override uint MapID
+        {
+            get
+            {
+                return 11016;
+            }
+        }
+
+        public override string MapName
+        {
+            get
+            {
+                return "Cruise Ship";
+            }
+        }
     }
 }
