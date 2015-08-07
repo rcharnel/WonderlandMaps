8e3188ba3e3d5184b4d6f93a922385856cdd2ceb
Rcharnel <lilr15@live.com>
2015-08-06 23:54:04 -0400

    added missing file

---
+++ 60000/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Plugin;
+
+namespace _60000
+{
+    public class map_60000 : GameMap
+    {
+        public map_60000(object host, object src)
+            : base((PluginHost)host, (System.IO.FileInfo)src)
+        {
+
+        }
+        
+        public override uint MapID
+        {
+            get
+            {
+                return 60000;
+            }
+        }
+
+        public override string MapName
+        {
+            get
+            {
+                return "North Island";
+            }
+        }
+    }
+}
