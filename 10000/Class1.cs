7d28e6d8f6ef2237369aac8035e857260c04785d
Tokiko846 <garionj94@msn.com>
2015-08-10 03:18:59 -0700

    Couldn't sleep, added a few new maps. Also learned to copy paste entire code than make small edits. I feel dumb for not thinking of it sooner.

---
+++ 10000/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Plugin;
+
+namespace _10000
+{
+    public class map10000:GameMap
+       
+    {
+        public map10000(object host, object src)
+             : base((PluginHost)host, (System.IO.FileInfo)src)
+        {
+
+        }
+
+        public override uint MapID
+        {
+            get
+            {
+                return base.MapID;
+            }
+
+            set
+            {
+                base.MapID = value;
+            }
+        }
+
+        public override string MapName
+        {
+            get
+            {
+                return base.MapName;
+            }
+        }
+
+        public override MapType Type
+        {
+            get
+            {
+                return base.Type;
+            }
+        }
+
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ 10070/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Plugin;
+
+namespace _10070
+{
+    public class map10070:GameMap
+    {
+        public map10070(object host, object src)
+            : base((PluginHost)host, (System.IO.FileInfo)src)
+        {
+
+        }
+
+        public override uint MapID
+        {
+            get
+            {
+                return base.MapID;
+            }
+
+            set
+            {
+                base.MapID = value;
+            }
+        }
+
+        public override string MapName
+        {
+            get
+            {
+                return base.MapName;
+            }
+        }
+
+        public override MapType Type
+        {
+            get
+            {
+                return base.Type;
+            }
+        }
+
+        public void Test ()
+        {
+
+        }
+    }
+}
