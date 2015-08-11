eb6aa7743ddf37ac3b8dfc90640f514820125624
Rcharnel <lilr15@live.com>
2015-08-10 23:30:52 -0400

    updated Dlls to use  .net 4.5 for now

---
+++ 60000/WarpPortal/ToSB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Maps;
+using Game;
+
+namespace _60000.WarpPortal
+{
+    public class ToSB : Game.Maps.WarpPortal
+    {
+        public ToSB()
+        {
+            this.clickID = 1;
+            this.DstID = 1;
+            accessBy = AccessFlags.Any;
+        }
+    }
+}
