459981abdeb678df644ba081855fd6a86cca614d
Tokiko846 <garionj94@msn.com>
2015-08-12 02:48:51 -0700

    Some more maps.

---
+++ 10004/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Plugin;
+
+namespace _10004
+{
+    public class map10004 : GameMap
+
+    {
+        public map10004(object host, object src)
+             : base((PluginHost)host, (System.IO.FileInfo)src)
+        {
+
+        }
+
+        public override uint MapID
+        {
+            get
+            {
+                return base.MapID;
+            }
+
+            set
+            {
+                base.MapID = value;
+            }
+        }
+
+        public override string MapName
+        {
+            get
+            {
+                return base.MapName;
+            }
+        }
+
+        public override MapType Type
+        {
+            get
+            {
+                return base.Type;
+            }
+        }
+
+        public void Test()
+        {
+
+        }
+    }
+}
