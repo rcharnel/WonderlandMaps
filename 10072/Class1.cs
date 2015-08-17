cfb6686f0465b2e67f4eb9691674d6ef1543eea3
Tokiko846 <garionj94@msn.com>
2015-08-16 18:39:29 -0700

    A ton more Maps. Look at 10087 and decide a better name for it though. It's actual name is §ÆÃ¾°g®c(´ú). So good luck.

---
+++ 10072/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Plugin;
+
+namespace _10072
+{
+    public class map10072 : GameMap
+
+    {
+        public map10072(object host, object src)
+             : base((PluginHost)host, (System.IO.FileInfo)src)
+        {
+
+        }
+
+        public override uint MapID
+        {
+            get
+            {
+                return 10072;
+            }
+
+            set
+            {
+                base.MapID = value;
+            }
+        }
+
+        public override string MapName
+        {
+            get
+            {
+                return "Survival Isle";
+            }
+        }
+
+        public override MapType Type
+        {
+            get
+            {
+                return base.Type;
+            }
+        }
+
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ 10076/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game;
+using Plugin;
+
+namespace _10076
+{
+    public class map10076 : GameMap
+
+    {
+        public map10076(object host, object src)
+             : base((PluginHost)host, (System.IO.FileInfo)src)
+        {
+
+        }
+
+        public override uint MapID
+        {
+            get
+            {
+                return 10076;
+            }
+
+            set
+            {
+                base.MapID = value;
+            }
+        }
+
+        public override string MapName
+        {
+            get
+            {
+                return "Treasure Room4";
+            }
+        }
+
+        public override MapType Type
+        {
+            get
+            {
+                return base.Type;
+            }
+        }
+
+        public void Test()
+        {
+
+        }
+    }
+}
